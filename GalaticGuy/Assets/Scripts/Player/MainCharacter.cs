31ab59d9a77d48ef6d9e7bf2fede84860c598b2c
Drkrtl <drkrthanlife@gmail.com>
2019-08-17 20:24:12 +0800

    Revert "charge meter added"
    This reverts commit 52d7b1f9efbbbd7731e3072b97d72ba79297d03f.

--- GalaticGuy/Assets/Scripts/Player/MainCharacter.cs
+++ GalaticGuy/Assets/Scripts/Player/MainCharacter.cs
@@ -8,16 +8,11 @@ public class MainCharacter : MonoBehaviour
     CharacterShoot shoot;
     CharacterHealth health;
 
-    int chargeMeter;
-    //50 seconds to charge (60 x 50)
-    const int CHARGEMETER_MAX = 3000;
-
     // Start is called before the first frame update
     void Start()
     {
         move = GetComponent<CharacterMovement>();
         shoot = GetComponent<CharacterShoot>();
-        chargeMeter = 0;
     }
 
     // Update is called once per frame
@@ -26,20 +21,8 @@ public class MainCharacter : MonoBehaviour
         //input direction x
         move.InputDirectionX(Mathf.RoundToInt(Input.GetAxis(Labels.Inputs.HORIZONTAL_AXIS)));
 
-    }
-
-    private void FixedUpdate()
-    {
         if (IsInput())
-        {
-            if(chargeMeter < CHARGEMETER_MAX)
-                shoot.TryShoot();
-        }
-        else
-        {
-            //increment charge when not moving
-            chargeMeter++;
-        }
+            shoot.TryShoot();
     }
 
     public bool IsInput()
