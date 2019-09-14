262c84cddf1d9c26e3c632eb5eb5bdc4111081fe
Drkr <drkrthanlife@gmail.com>
2019-09-15 00:53:38 +0800

    added some shop ui stuff

---
+++ GalaticGuy/Assets/Scripts/Player/Upgrades/PlayerUpgrade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerUpgrade
+{
+    private CharacterMovement movement;
+    protected MainCharacter player;
+    private CharacterShoot shoot;
+
+    public void Initialise(CharacterMovement movement, MainCharacter player, CharacterShoot shoot)
+    {
+        this.movement = movement;
+        this.player = player;
+        this.shoot = shoot;
+    }
+
+    //every frame update enemy, give character info and positional info
+    public abstract void UpdateFrame(Vector3 currentPosition);
+
+    //
+    public void OnMove(Vector3 direction)
+    {
+        
+    }
+
+    public void OnShoot(Vector3 direction)
+    {
+
+    }
+
+    public void OnRemove()
+    {
+
+    }
+}
