eb4fc51c6425eb0d6f75e6a491a1cb6b51802284
Drkrtl <drkrthanlife@gmail.com>
2019-11-07 22:00:03 +0800

    Changed spawning algorithm, only finishes after spawn queue is empty and all enemies are dead

--- GalaticGuy/Assets/Scripts/GameManagement/EnemySpawner.cs
+++ GalaticGuy/Assets/Scripts/GameManagement/EnemySpawner.cs
@@ -7,15 +7,16 @@ public class EnemySpawner : MonoBehaviour
     public static EnemySpawner INST = null;
 
     [SerializeField]
-    float spawnPositionY = 5;
+    float spawnPositionY = 6;
     [SerializeField]
     Level[] levels;
     int currentLevel;
 
+
     int killedCount = 0;
     int spawnedCount = 0;
 
-    int timeTillNext = 0;
+    int timeTillNextSpawn = 0;
     const int MAXTIME = 450;
     const int MINTIME = 10;
     const int REROLL_REDUCER = 13;
@@ -28,6 +29,8 @@ public class EnemySpawner : MonoBehaviour
     public struct Tier
     {
         [SerializeField]
+        public string name;
+        [SerializeField]
         public Enemy[] enemiesPrefabs;
         [SerializeField]
         public Enemy[] bossPrefabs;
@@ -39,12 +42,20 @@ public class EnemySpawner : MonoBehaviour
     public struct Squad
     {
         [System.Serializable]
-        public struct EnemyInfo
+        public struct SquadEnemy
         {
             public Enemy enemy;
-            public Vector2 relativePosition;
+            public Vector2 spawnPosition;
         }
-        public EnemyInfo[] squadMembers;
+
+        public SquadEnemy[] squadMembers;
+    }
+
+    [System.Serializable]
+    public struct EnemyInfo
+    {
+        public Enemy enemy;
+        public int delay;
     }
 
     [SerializeField]
@@ -54,6 +65,7 @@ public class EnemySpawner : MonoBehaviour
     private EndLevelDelegate endListener;
 
     private bool ended = false;
+    Queue<EnemyInfo> enemySpawnQueue = new Queue<EnemyInfo>();
 
     // Start is called before the first frame update
     void Start()
@@ -62,8 +74,11 @@ public class EnemySpawner : MonoBehaviour
             INST = this;
         else
             Destroy(this);
-        currentLevel = 0;
+
+        currentLevel = -1;
         killedCount = 0;
+
+        StartLevel();
     }
 
     // Update is called once per frame
@@ -72,9 +87,9 @@ public class EnemySpawner : MonoBehaviour
         //boss levels
         if (levels[currentLevel].containsBoss)
         {
-            if(spawnedCount <= 0)
+            if (spawnedCount <= 0)
                 SpawnRandomBoss(levels[currentLevel].difficulty);
-            else if(killedCount > 0 && !ended)
+            else if (killedCount > 0 && !ended)
             {
                 ended = true;
                 OnLevelEnd();
@@ -84,30 +99,48 @@ public class EnemySpawner : MonoBehaviour
         //other level
         else
         {
-            if (spawnedCount < levels[currentLevel].length)
-            {
-                if (timeTillNext <= 0)
-                {
-                    SpawnRandomEnemy(levels[currentLevel].difficulty);
-                    //get next spawn time
-                    if (spawnedCount < levels[currentLevel].length)
-                    {
-                        timeTillNext = GetNextTime(levels[currentLevel]);
-                        Debug.Log("time till next spawn " + timeTillNext);
-                    }
-                }
-                else
-                {
-                    timeTillNext--;
-                }
-            }
-            else if (!ended && killedCount >= levels[currentLevel].length)
+            if (!ended &&
+                enemySpawnQueue.Count <= 0 &&
+                killedCount >= spawnedCount)
             {
                 ended = true;
                 OnLevelEnd();
                 Debug.Log("spawning stopped");
             }
         }
+    }
+
+    private void AddEnemyToQueue(Enemy prefab, Vector3 position)
+    {
+        Enemy enemy = SpawnEnemy(prefab, position);
+        enemy.gameObject.SetActive(false);
+        EnemyInfo info = new EnemyInfo();
+
+        info.enemy = enemy;
+        info.delay = GetNextTime(levels[currentLevel]);
+        //info.spawnPosition = spawnpos;
+
+        enemySpawnQueue.Enqueue(info);
+
+        Debug.Log(enemy.gameObject.name + ", " + info.delay);
+    }
+
+    private void SpawnNextEnemy()
+    {
+        if (enemySpawnQueue.Count > 0)
+        {
+            EnemyInfo enemyInfo = enemySpawnQueue.Dequeue();
+            StartCoroutine(WaitForNextSpawn(enemyInfo));
+        }
+    }
+
+    private IEnumerator WaitForNextSpawn(EnemyInfo enemyInfo)
+    {
+        float delay = enemyInfo.delay / 60f;
+        yield return new WaitForSeconds(delay);
+        enemyInfo.enemy.gameObject.SetActive(true);
+        //enemyInfo.enemy.transform.position = enemyInfo.spawnPosition;
+        SpawnNextEnemy();
     }
 
     private int GetNextTime(Level level)
@@ -124,7 +157,8 @@ public class EnemySpawner : MonoBehaviour
                 unclampedTime = unclampedTime - Random.Range(0, level.difficulty + 1) * DIFF_MULTI;
             }
         }
-        //remove difficulty
+
+        //remove difficulty??
 
         int nextTime = Mathf.Clamp(unclampedTime, MINTIME, MAXTIME);
 
@@ -166,24 +200,31 @@ public class EnemySpawner : MonoBehaviour
     {
         int spawnIndex = Random.Range(0, tiers[difficulty].squads.Length);
         Squad squad = tiers[difficulty].squads[spawnIndex];
-        Vector2 squadPosition = new Vector2(Random.Range(-4f, 4f), spawnPositionY);
+        Vector2 squadPosition = GetRandomSpawnPos();
 
-
-        foreach (Squad.EnemyInfo enemyIn in squad.squadMembers)
+        foreach (Squad.SquadEnemy enemyIn in squad.squadMembers)
         {
-            SpawnEnemy(enemyIn.enemy, squadPosition + enemyIn.relativePosition);
+            //SpawnEnemy(enemyIn.enemy, squadPosition + enemyIn.spawnPosition);
+            //SpawnEnemy()
         }
     }
 
-    private Enemy SpawnRandomEnemy(int difficulty)
+    private Enemy GetRandomEnemyPrefab(int difficulty)
     {
-        Enemy enemy;
-        Vector3 spawnPosition = new Vector3(Random.Range(-4f, 4f), spawnPositionY);
+        //Enemy enemy;
+        //Vector3 spawnPosition = new Vector3(Random.Range(-4f, 4f), spawnPositionY);
         int spawnIndex = Random.Range(0, tiers[difficulty].enemiesPrefabs.Length);
 
-        enemy = SpawnEnemy(tiers[difficulty].enemiesPrefabs[spawnIndex], spawnPosition);
+        //enemy = SpawnEnemy(tiers[difficulty].enemiesPrefabs[spawnIndex], spawnPosition);
 
-        return enemy;
+        return tiers[difficulty].enemiesPrefabs[spawnIndex];
+    }
+
+
+    //gives position inside spawn bounds
+    public Vector3 GetRandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-CharacterMovement.xBound, CharacterMovement.xBound), spawnPositionY);
     }
 
     public Enemy SpawnEnemy(Enemy enemy, Vector3 position)
@@ -209,6 +250,15 @@ public class EnemySpawner : MonoBehaviour
         spawnedCount = 0;
         killedCount = 0;
         ended = false;
+
+        int enemiesToSpawn = levels[currentLevel].length;
+
+        for(int ii = 0; ii < enemiesToSpawn; ii++)
+        {
+            Enemy enemy = GetRandomEnemyPrefab(levels[currentLevel].difficulty);
+            AddEnemyToQueue(enemy, GetRandomSpawnPos());
+        }
+        SpawnNextEnemy();
     }
 
     public void EnemyDied()
