89040cf630fd74deebc0fe56eb6e29728b5d3bcc
Drkrtl <drkrthanlife@gmail.com>
2019-11-17 22:54:43 +0800

    made small swarm units move directly towards player

---
+++ GalaticGuy/Assets/Scripts/Enemy/AiObjects/AIDown.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDown : EnemyAI
+{
+    //tries to shoot and move every frame
+    public override void UpdateFrame(Vector3 currentPosition)
+    {
+        Move(Vector3.down);
+    }
+}
