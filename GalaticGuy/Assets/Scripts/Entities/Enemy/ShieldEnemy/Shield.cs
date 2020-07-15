bec3d57b7f253e18442e59a4af2984d1abc318bc
Drkr <drkrthanlife@gmail.com>
2020-07-15 21:09:51 +0800

    renaming, shield enemy things

---
+++ GalaticGuy/Assets/Scripts/Entities/Enemy/ShieldEnemy/Shield.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour, IDamageable
+{
+    [SerializeField]
+    int baseHP = 5;
+
+    int hp = 5;
+    float percentHP;
+    SpriteRenderer sprite;
+
+    private void OnEnable()
+    {
+        hp = baseHP;
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void IDamageable.OnDamage(int inDamage)
+    {
+        hp -= inDamage;
+
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            percentHP = (float)hp / (float)baseHP;
+            sprite.color = new Color(1f, 1f, 1f, percentHP);
+        }
+    }
+}
---
+++ GalaticGuy/Assets/Scripts/Entities/Projectile.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour, IDamageable
+{
+    [System.Serializable]
+    protected class Stats
+    {
+        public int damage;
+        public float initialSpeed;
+        public float yAccelerationOverTime = 0f;
+        [Range(1f,1000f)]
+        public float dragOverTime = 1f;
+
+        public int maxLifeTime = DEFAULT_LIFETIME;
+
+        public int hp = 1;
+        public const int DEFAULT_LIFETIME = 180;
+    }
+    protected int timeSinceBirth = 0;
+    protected int index;
+
+    Rigidbody2D rb;
+    protected Vector2 velocity;
+    protected Shooter parent;
+
+    [SerializeField]
+    protected Stats stats;
+
+    [Header("Collisions")]
+    [SerializeField]
+    bool player = true;
+    [SerializeField]
+    bool projectiles = true;
+    [SerializeField]
+    bool enemies = true;
+    [Space(0.1f)]
+    [SerializeField]
+    bool destroyOnHit = true;
+    [SerializeField]
+    AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (stats.maxLifeTime <= 0)
+            stats.maxLifeTime = Stats.DEFAULT_LIFETIME;
+        rb = GetComponent<Rigidbody2D>();
+        if(audioClip != null)
+            GameManager.audioManager.CreateReplacableAudio(audioClip);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        velocity = GetVelocity();
+        rb.velocity = velocity;
+        timeSinceBirth++;
+
+        if (timeSinceBirth > stats.maxLifeTime)
+            DisableObject();
+    }
+
+    protected Vector2 GetVelocity()
+    {
+        Vector2 drag = (velocity / stats.dragOverTime);
+        Vector2 accelerated = drag + Vector2.up* stats.yAccelerationOverTime;
+        
+        return (accelerated*(Time.fixedDeltaTime)  + velocity * (1f-Time.fixedDeltaTime));
+    }
+
+    public void Initialise(int index, Shooter parent)
+    {
+        this.index = index;
+        this.parent = parent;
+        OnInitialise();
+    }
+
+    public virtual void OnInitialise()
+    {
+        //instantiated by children
+    }
+
+    public void Activate(Vector3 position, Vector2 direction)
+    {
+        this.velocity = direction.normalized * stats.initialSpeed;
+        transform.position = position;
+        timeSinceBirth = 0;
+        gameObject.SetActive(this);
+        //Debug.Log(velocity+ ", "+direction+", "+stats.initialSpeed);
+    }
+
+    //default behaviour: destroy on hit
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CheckEnemy(collision) ||
+            CheckPlayer(collision) ||
+            CheckProj(collision))
+        {
+            //damage hit target
+            IDamageable damageable = collision.GetComponentInParent<IDamageable>();
+            damageable?.OnDamage(stats.damage);
+            //disable this object
+            if(!CheckProj(collision))
+                OnDamage(stats.hp);
+        }
+    }
+
+    private bool CheckTag(Collider2D col, string tag)
+    {
+        return col.tag.Equals(tag);
+    }
+
+    public bool CheckProj(Collider2D col)
+    {
+        return CheckTag(col, Labels.Tags.PROJECTILE) && projectiles;
+    }
+
+    public bool CheckEnemy(Collider2D col)
+    {
+        return CheckTag(col, Labels.Tags.ENEMY) && enemies;
+    }
+
+    public bool CheckPlayer(Collider2D col)
+    {
+        return CheckTag(col, Labels.Tags.PLAYER) && player;
+    }
+
+    //Destroys 
+    public virtual void OnDamage(int inDamage)
+    {
+        stats.hp--;
+        if (destroyOnHit && stats.hp <= 0)
+            DisableObject();
+    }
+
+    public virtual void DisableObject()
+    {
+        parent.DisableProjectile(this);
+    }
+
+    public int GetIndex()
+    {
+        return index;
+    }
+}
