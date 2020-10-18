6cf80e64d3bcabc817600235794a07acc9998ac2
Drkr <drkrthanlife@gmail.com>
2020-10-18 20:55:08 +0800

    removed first level charge sound

--- GalaticGuy/Assets/Scripts/UI/UICharge.cs
+++ GalaticGuy/Assets/Scripts/UI/UICharge.cs
@@ -67,8 +67,6 @@ public class UICharge : MonoBehaviour
 
         if(chargebarR.value >= 1f && !chargeBarRFilled.enabled)
         {
-            GameManager.AudioEvents.PlayAudio(filledClip);
-
             chargeBarRFilled.enabled = true;
         }
 
